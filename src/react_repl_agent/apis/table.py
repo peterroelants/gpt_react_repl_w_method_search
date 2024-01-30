43a157ee36927d7d1599a2dd7a94ece58cb70612
Peter Roelants <Peter.roelants@gmail.com>
2024-01-30 19:56:25 +0100

    Komment quickstart demo

--- src/react_repl_agent/apis/table.py
+++ src/react_repl_agent/apis/table.py
@@ -127,3 +127,4 @@ def run_sql_query(table: Table, sql: str) -> Table:
             "\nMake sure the SQL query is correct! Pre-format all variables in the query string and use quotes if needed."
         ) from exc
     return Table(result_df)
+
